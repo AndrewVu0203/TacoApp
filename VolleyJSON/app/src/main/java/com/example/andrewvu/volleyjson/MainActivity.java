cb15f2b697aeee495b72c8dec1688e29c2162888
andrew <andrewvu0203@gmail.com>
2019-04-13 17:55:39 -0500

    VolleyJSON error

--- VolleyJSON/app/src/main/java/com/example/andrewvu/volleyjson/MainActivity.java
+++ VolleyJSON/app/src/main/java/com/example/andrewvu/volleyjson/MainActivity.java
@@ -2,12 +2,72 @@ package com.example.andrewvu.volleyjson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
     }
+
+    private void jsonParse(){
+        String url = "https://api.myjson.com/bins/1g29a4";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("employees");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject employee = jsonArray.getJSONObject(i);
+
+                        String firstName = employee.getString("firstname");
+                        int age = employee.getInt("age");
+                        String mail = employee.getString("mail");
+
+                        mTextViewResult.append(firstName + ", " + String.valueOf(age) + ", " + mail + "\n\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+
+    }
+
 }
