233d1d237a29ab7b3fd69f4440c58e8d582733b3
andrew <andrewvu0203@gmail.com>
2019-04-13 22:01:24 -0500

    fix itemTextView

--- RowdyHackApp/app/src/main/java/com/example/andrewvu/rowdyhackapp/MainActivity.java
+++ RowdyHackApp/app/src/main/java/com/example/andrewvu/rowdyhackapp/MainActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    // it is completely the same, but TextView different only
 
-    //vars
+    private static final String TAG = "MainActivity";
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<String> mItemTextView = new ArrayList<>();
+    private TextView itemTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,37 +29,56 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: started.");
 
         initImageBitmaps();
-
     }
 
     private void initImageBitmaps(){
         Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
         mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
         mNames.add("Havasu Falls");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
         mNames.add("Trondheim");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
         mNames.add("Portugal");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
         mNames.add("Rocky Mountain National Park");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
         mNames.add("Mahahual");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
         mNames.add("Frozen Lake");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
         mNames.add("White Sands Desert");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
         mNames.add("Austrailia");
+        mItemTextView.add("Havasu Falls");
+
         mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
         mNames.add("Washington");
+        mItemTextView.add("Havasu Falls");
+
         initRecyclerView();
     }
 
     private void initRecyclerView(){
         Log.d(TAG, "initRecyclerView: init recyclerview.");
         RecyclerView recyclerView = findViewById(R.id.recyclerv_view);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls, mItemTextView);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
+
--- RowdyHackApp/app/src/main/java/com/example/andrewvu/rowdyhackapp/RecyclerViewAdapter.java
+++ RowdyHackApp/app/src/main/java/com/example/andrewvu/rowdyhackapp/RecyclerViewAdapter.java
@@ -1,6 +1,8 @@
 package com.example.andrewvu.rowdyhackapp;
 
+
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,10 +14,16 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+/**
+ * Created by User on 1/1/2018.
+ */
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
 
@@ -23,50 +31,65 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private ArrayList<String> mImageNames = new ArrayList<>();
     private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mItemTextView = new ArrayList<>();
     private Context mContext;
 
+    public RecyclerViewAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> images, ArrayList<String> itemText ) {
+        mImageNames = imageNames;
+        mImages = images;
+        mContext = context;
+        mItemTextView = itemText;
+    }
 
-    public RecyclerViewAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> images) {
-            mImageNames = imageNames;
-            mImages = images;
-            mContext = context;
-        }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_lisitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
 
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_lisitem, parent, false);
-            ViewHolder holder = new ViewHolder(view);
-            return holder;
-        }
-        @Override
-        public void onBindViewHolder(ViewHolder holder, final int position) {
-            Log.d(TAG, "onBindViewHolder: called.");
-            Glide.with(mContext)
-                    .asBitmap()
-                    .load(mImages.get(position))
-                    .into(holder.image);
-            holder.imageName.setText(mImageNames.get(position));
-            holder.parentLayout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.d(TAG, "onClicked: clicked on: " + mImageNames.get(position));
-                    Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        @Override
-        public int getItemCount() {
-            return mImageNames.size();
-        }
-        public class ViewHolder extends RecyclerView.ViewHolder{
-            CircleImageView image;
-            TextView imageName;
-            RelativeLayout parentLayout;
-            public ViewHolder(View itemView) {
-                super(itemView);
-                image = itemView.findViewById(R.id.image);
-                imageName = itemView.findViewById(R.id.image_name);
-                parentLayout = itemView.findViewById(R.id.parent_layout);
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.image);
+
+        holder.imageName.setText(mImageNames.get(position));
+        holder.itemTextView.setText(mItemTextView.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+
+                Toast.makeText(mContext, mImageNames.get(position), Toast.LENGTH_SHORT).show();
             }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+        TextView itemTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+            itemTextView = itemView.findViewById(R.id.itemTextView);
+
         }
     }
+}
