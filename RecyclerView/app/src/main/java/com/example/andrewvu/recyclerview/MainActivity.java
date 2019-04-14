5ce7fdcb3c52b5915c1a207ae2094bac6c2885ba
andrew <andrewvu0203@gmail.com>
2019-04-13 19:49:48 -0500

    error
    delete initImageBitmaps(),
    delte initRecyclerView(),
    have error cause mTextView is not on Main.

--- RecyclerView/app/src/main/java/com/example/andrewvu/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/andrewvu/recyclerview/MainActivity.java
@@ -4,6 +4,20 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -11,59 +25,97 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    //vars
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
+    private TextView mTextView;
+    private TextView mTextView1;
+    private TextView mTextView2;
+    private Button mParseButton;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.d(TAG, "onCreate: started.");
 
-        initImageBitmaps();
+        mTextView = findViewById(R.id.image_name);
+        mTextView1 = findViewById(R.id.image_name1);
+        mTextView2 = findViewById(R.id.image_name2);
+        mParseButton = findViewById(R.id.buttonParse);
+
+        mQueue = Volley.newRequestQueue(this);
+        mParseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getJSONFromFirebase();
+            }
+        });
+
+        // TODO: Set the Text to TextView in layout_listitem
     }
 
-    private void initImageBitmaps(){
-        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+    private void getJSONFromFirebase() {
+        // TODO (now) : jsonParse changeName
+        String url = "https://api.myjson.com/bins/hj470";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("employees");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject employee = jsonArray.getJSONObject(i);
+
+                        String firstName = employee.getString("firstname");
+                        int age = employee.getInt("age");
+                        String mail = employee.getString("mail");
+
+                        mTextView.append(firstName + ", " + String.valueOf(age) + ", " + mail + "\n\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
 
         mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
         mNames.add("Havasu Falls");
 
         mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
+        mNames.add("Havasu Falls");
 
         mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
+        mNames.add("Havasu Falls");
 
         mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mNames.add("Rocky Mountain National Park");
-
+        mNames.add("Havasu Falls");
 
         mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mNames.add("Mahahual");
+        mNames.add("Havasu Falls");
 
         mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
-        mNames.add("Frozen Lake");
+        mNames.add("Havasu Falls");
 
-
-        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-        mNames.add("White Sands Desert");
-
-        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
-        mNames.add("Austrailia");
-
-        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-        mNames.add("Washington");
-
-        initRecyclerView();
     }
 
-    private void initRecyclerView(){
+    private void initRecyclerViewJSON() {
+        // TODO: put JSON data to RecyclerView
+        // TODO: modify RecyclerViewAdapter
+
         Log.d(TAG, "initRecyclerView: init recyclerview.");
         RecyclerView recyclerView = findViewById(R.id.recyclerv_view);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
